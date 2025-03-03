969d4e41eb9fff30064aadf68922905b8649610c
IClarisse <c.iradukund4@alustudent.com>
2025-03-03 01:09:40 -0800

    script 7

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def islower(c):
+    return ord(c) >= ord('a') and ord(c) <= ord('z')
+
+# Test cases for checking the function
+def main():
+    test_cases = ['a', 'z', 'A', 'Z', '4', '!']
+    
+    for char in test_cases:
+        if islower(char):
+            print(f"{char} => lower")
+        else:
+            print(f"{char} => upper")
+
+if __name__ == "__main__":
+    main()
