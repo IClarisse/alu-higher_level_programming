8a00dcf6e247603f7e88ded02af7707082a77756
IClarisse <c.iradukund4@alustudent.com>
2025-03-06 09:29:33 -0800

    7 fixed

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,12 +2,12 @@
 def update_dictionary(a_dictionary, key, value):
     """
     Replaces or adds key/value in a dictionary
-    
+
     Args:
         a_dictionary: the dictionary to modify
         key: the key to add or update (always a string)
         value: the value to set (any type)
-        
+
     Returns:
         The updated dictionary
     """
