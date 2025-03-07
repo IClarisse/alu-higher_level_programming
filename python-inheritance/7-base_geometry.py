e7756fe94670f80d2ec61eed7fb26bc1db08783a
IClarisse <c.iradukund4@alustudent.com>
2025-03-07 21:56:30 +0200

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
 This module defines the class BaseGeometry.
-The class contains the method 'area' that raises an exception
-indicating that it is not implemented, and an 'integer_validator'
-method to validate integers based on the specified conditions.
+The class contains:
+    - area() method: raises an exception for not being implemented.
+    - integer_validator() method: validates integers based on provided conditions.
 """
+
 class BaseGeometry:
     """
     A BaseGeometry class that serves as a base for other geometrical shapes.
@@ -35,28 +36,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-
-if __name__ == "__main__":
-    # Create an instance of BaseGeometry
-    bg = BaseGeometry()
-
-    # Valid integer validation
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
-    # Test invalid values
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
