e9a75dae7f462adb70bf93848141502dd5920a18
IClarisse <c.iradukund4@alustudent.com>
2025-03-07 21:44:37 +0200

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""
+This module defines the class BaseGeometry.
+The class is an empty base class with no functionality.
+"""
 
 class BaseGeometry:
+    """
+    A BaseGeometry class that serves as a base for other geometrical shapes.
+    Currently, it does not contain any attributes or methods.
+    """
     pass
+
+
+if __name__ == "__main__":
+    # Create an instance of BaseGeometry
+    bg = BaseGeometry()
+
+    # Print the instance of the object
+    print(bg)
+
+    # Print the list of attributes and methods of the instance
+    print(dir(bg))
+
+    # Print the list of attributes and methods of the class itself
+    print(dir(BaseGeometry))
