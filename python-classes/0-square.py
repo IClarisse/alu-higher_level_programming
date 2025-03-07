ca230ab148173a5cd4b5a3448a6f13ef5fcbfae0
IClarisse <c.iradukund4@alustudent.com>
2025-03-07 10:13:44 -0800

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+"""
+This module defines an empty Square class
+"""
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """
+    Empty class that defines a square
+    """
+    pass
