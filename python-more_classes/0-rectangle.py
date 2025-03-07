fffeb5935fb1b8e574eb9712b2ac842f8e1d741e
IClarisse <c.iradukund4@alustudent.com>
2025-03-07 08:01:11 -0800

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+ """
+This module defines an empty class `Rectangle`.
+
+The `Rectangle` class serves as a placeholder for a rectangle object,
+which could be expanded in the future with attributes (e.g., width, height)
+and methods (e.g., area, perimeter). The current version of this class is empty
+and does not define any attributes or methods.
+
+This class is intended to be a starting point for further implementation.
+"""
+
+class Rectangle:
+    """
+    An empty class representing a rectangle.
+
+    This class currently has no attributes or methods.
+    It serves as a placeholder for a rectangle that could be expanded
+    in the future with attributes like width, height, etc., and methods
+    such as calculating the area or perimeter.
+    """
+    pass
