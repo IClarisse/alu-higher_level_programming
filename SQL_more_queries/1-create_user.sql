8a3666179dbd50037272348ec306f0cf164c494d
IClarisse <c.iradukund4@alustudent.com>
2025-03-13 22:21:48 -0700

    Fixed script 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
--- create use if not there 
+-- Create user_0d_1 only if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+
+-- Grant all privileges on the server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes
+FLUSH PRIVILEGES;
